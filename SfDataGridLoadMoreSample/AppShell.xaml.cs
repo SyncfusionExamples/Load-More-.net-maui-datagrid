4f984daae978408ba2f7c98820200538cdfcdd75
Nirmalkumar <nirmalkumar.yuvaraj@syncfusion.com>
2024-02-02 11:07:06 +0530

    Added sample for LoadMore

---
+++ SfDataGridLoadMoreSample/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SfDataGridLoadMoreSample
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
